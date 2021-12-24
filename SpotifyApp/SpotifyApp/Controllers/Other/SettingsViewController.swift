ff1855175177c1cddb475a34181c0bc5e46c5b1c
Ekrem <ekrem@3pounds.io>
2021-12-24 12:23:18 +0300

    Sign out completed.

--- SpotifyApp/SpotifyApp/Controllers/Other/SettingsViewController.swift
+++ SpotifyApp/SpotifyApp/Controllers/Other/SettingsViewController.swift
@@ -46,7 +46,26 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     private func signOutTapped() {
-        
+        let alert = UIAlertController(title: "Sign Out",
+                                      message: "Are you sure?",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
+            AuthManager.shared.signOut { [weak self] signedOut in
+                if signedOut {
+                    DispatchQueue.main.async {
+                        let navVC = UINavigationController(rootViewController: WelcomeViewController())
+                        navVC.navigationBar.prefersLargeTitles = true
+                        navVC.viewControllers.first?.navigationItem.largeTitleDisplayMode = .always
+                        navVC.modalPresentationStyle = .fullScreen
+                        self?.present(navVC, animated: true, completion: {
+                            self?.navigationController?.popToRootViewController(animated: false)
+                        })
+                    }
+                }
+            }
+        }))
+        present(alert, animated: true)
     }
     
     private func viewProfile() {
--- SpotifyApp/SpotifyApp/Managers/AuthManager.swift
+++ SpotifyApp/SpotifyApp/Managers/AuthManager.swift
@@ -172,4 +172,11 @@ final class AuthManager {
         UserDefaults.standard.setValue(Date().addingTimeInterval(TimeInterval(result.expires_in)), forKey: "expirationDate")
          
     }
+    public func signOut(completion: (Bool) -> Void) {
+            UserDefaults.standard.setValue(nil, forKey: "access_token")
+            UserDefaults.standard.setValue(nil, forKey: "refresh_token")
+            UserDefaults.standard.setValue(nil, forKey: "expirationDate")
+
+            completion(true)
+        }
 }
